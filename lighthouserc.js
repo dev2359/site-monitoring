5b6a050a437c292893c6759fa7237bdb699ac414
dev <142872162+dev2359@users.noreply.github.com>
2026-04-27 09:50:56 +0900

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -39,7 +39,6 @@ module.exports = {
       	'https://celladix.sg',
       	'https://celladix.sg/products/https-celladix-sg-products-celladix-131-pore-clearing-serum',
       	'https://lactomedi.jp',
-      	'https://lactomedi.jp/products/%E3%83%91%E3%83%92%E3%83%A5%E3%83%BC%E3%83%A0%E3%83%9F%E3%82%B9%E3%83%88',
       	'https://celladix.jp',
       	'https://celladix.jp/products/sebum-rebalancing-rx-131-ampoule',
        ],
